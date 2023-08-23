aeb25e1aa7947229eed03a812359ab845dd4f65b
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-08-23 11:59:04 +0530

    Add filter capability

--- meteorhub/src/components/SearchComponent.jsx
+++ meteorhub/src/components/SearchComponent.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
-import Data from '../assets/data.json';
+import React, { useState, useEffect } from "react";
 
 export default function SearchComponent() {
-    const [query, setQuery] = useState("");
-    console.log(Data);
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+    useEffect(()=>{
+        fetch('./src/assets/data.json')
+            .then(response => response.json())
+            .then(jsonData =>{
+                setData(jsonData);
+                setFilteredData(jsonData);
+            })
+            .catch(error=>console.error("Error fetching data:", error));
+    }, []);
+    useEffect(()=>{
+        const filteredResults = data.filter(item => 
+            item.name.toLowerCase().includes(query.toLowerCase()) || 
+            item.year.toLowerCase().includes(query.toLowerCase()) || 
+            item.recclass.toLowerCase().includes(query.toLowerCase())
+        );
+        setFilteredData(filteredResults);
+    },[data, query]);
     return (
         <>
-            <input type="text" placeholder="Search" onChange={event => setQuery(event.target.value)} />
-            {Data.map((meteorData, id) => (
+            <input type="text" placeholder="Search" value={query} onChange={e => setQuery(e.target.value)} />
+            {filteredData.map((meteorData, id) => (
                 <div key={id}>
-                    <p>{meteorData.name}</p>
+                    <p>{meteorData.name} | {meteorData.year} | {meteorData.recclass} | {meteorData.mass}</p>  
                 </div>
             ))}
         </>
     )
 }
-
