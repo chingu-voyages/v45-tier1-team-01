704407c2700b6531efcb48bc999cd58fc3d6b4c2
CharanMN7_pc <maverickid247@gmail.com>
2023-08-29 19:33:40 +0530

    Updated the search bar css
    - Added SearchComponent.css file
    - Added the corresponding css file import in SearchComponent.jsx
    - Made slight changes to the seach bar looks

--- meteorhub/src/components/SearchComponent.jsx
+++ meteorhub/src/components/SearchComponent.jsx
@@ -1,34 +1,44 @@
 import React, { useState, useEffect } from "react";
+import "./SearchComponent.css";
 
 export default function SearchComponent() {
-    const [data, setData] = useState([]);
-    const [query, setQuery] = useState('');
-    const [filteredData, setFilteredData] = useState([]);
-    useEffect(()=>{
-        fetch('./src/assets/data.json')
-            .then(response => response.json())
-            .then(jsonData =>{
-                setData(jsonData);
-                setFilteredData(jsonData);
-            })
-            .catch(error=>console.error("Error fetching data:", error));
-    }, []);
-    useEffect(()=>{
-        const filteredResults = data.filter(item => 
-            item.name.toLowerCase().includes(query.toLowerCase()) || 
-            item.year.toLowerCase().includes(query.toLowerCase()) || 
-            item.recclass.toLowerCase().includes(query.toLowerCase())
-        );
-        setFilteredData(filteredResults);
-    },[data, query]);
-    return (
-        <>
-            <input type="text" placeholder="Search" value={query} onChange={e => setQuery(e.target.value)} />
-            {filteredData.map((meteorData, id) => (
-                <div key={id}>
-                    <p>{meteorData.name} | {meteorData.year} | {meteorData.recclass} | {meteorData.mass}</p>  
-                </div>
-            ))}
-        </>
-    )
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  useEffect(() => {
+    fetch("./src/assets/data.json")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setData(jsonData);
+        setFilteredData(jsonData);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  useEffect(() => {
+    const filteredResults = data.filter(
+      (item) =>
+        item.name.toLowerCase().includes(query.toLowerCase()) ||
+        item.year.toLowerCase().includes(query.toLowerCase()) ||
+        item.recclass.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredData(filteredResults);
+  }, [data, query]);
+  return (
+    <>
+      <input
+        id="search-bar"
+        type="text"
+        placeholder="Search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {filteredData.map((meteorData, id) => (
+        <div key={id}>
+          <p>
+            {meteorData.name} | {meteorData.year} | {meteorData.recclass} | {meteorData.mass}
+          </p>
+        </div>
+      ))}
+    </>
+  );
 }
