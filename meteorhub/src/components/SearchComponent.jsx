7cde7a88666517175c9bc1f8db1b128bde7cbb6c
Ashiya Banu <ashiya1005@gmail.com>
2023-08-30 18:54:31 +0530

    add clear button

--- meteorhub/src/components/SearchComponent.jsx
+++ meteorhub/src/components/SearchComponent.jsx
@@ -4,29 +4,31 @@ export default function SearchComponent() {
     const [data, setData] = useState([]);
     const [query, setQuery] = useState('');
     const [filteredData, setFilteredData] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('./src/assets/data.json')
             .then(response => response.json())
-            .then(jsonData =>{
+            .then(jsonData => {
                 setData(jsonData);
                 setFilteredData(jsonData);
             })
-            .catch(error=>console.error("Error fetching data:", error));
+            .catch(error => console.error("Error fetching data:", error));
     }, []);
-    useEffect(()=>{
-        const filteredResults = data.filter(item => 
-            item.name.toLowerCase().includes(query.toLowerCase()) || 
-            item.year.toLowerCase().includes(query.toLowerCase()) || 
+    useEffect(() => {
+        const filteredResults = data.filter(item =>
+            item.name.toLowerCase().includes(query.toLowerCase()) ||
+            item.year.toLowerCase().includes(query.toLowerCase()) ||
             item.recclass.toLowerCase().includes(query.toLowerCase())
         );
         setFilteredData(filteredResults);
-    },[data, query]);
+    }, [data, query]);
+
     return (
         <>
             <input type="text" placeholder="Search" value={query} onChange={e => setQuery(e.target.value)} />
+            <button className="clear" onClick={e => setQuery('')}>Clear</button>
             {filteredData.map((meteorData, id) => (
                 <div key={id}>
-                    <p>{meteorData.name} | {meteorData.year} | {meteorData.recclass} | {meteorData.mass}</p>  
+                    <p>{meteorData.name} | {meteorData.year} | {meteorData.recclass} | {meteorData.mass}</p>
                 </div>
             ))}
         </>
