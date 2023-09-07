20ce1c59ffa93cf22d4a267133a6019008c0408a
CharanMN7_pc <maverickid247@gmail.com>
2023-09-07 15:01:23 +0530

    Update PopUp.jsx
    Updated the HTML skeleton for the PopUp card

--- meteorhub/src/components/PopUp.jsx
+++ meteorhub/src/components/PopUp.jsx
@@ -3,24 +3,30 @@ export default function PopUp() {
   return (
     <aside id="overlay">
       <div id="pop-up-card">
-        <span>
-          <span id="meteor-name"></span>
+        <span className="meteor-prop biggie">
+          <span id="meteor-name">Abee</span>
           <button>X</button>
         </span>
-        <span className="meteor-prop">
-          <span className="the-prop"></span>
-          <span className="the-value"></span>
+        <span>
+          <span className="meteor-prop">
+            <span className="the-prop">Impact: </span>
+            <progress min={0} max={100} value={32}>
+              {/* a function that calculates the impact is needed for the value attribute  */}
+            </progress>
+          </span>
+          <span className="meteor-prop">
+            <span className="the-prop">Class:</span>
+            <span className="the-value">EH4</span>
+          </span>
+          <span className="meteor-prop">
+            <span className="the-prop">Mass:</span>
+            <span className="the-value">107000 MT</span>
+          </span>
+          <span className="meteor-prop">
+            <span className="the-prop">Year</span>
+            <span className="the-value">1952</span>
+          </span>
         </span>
-        <span className="meteor-prop">
-          <span className="the-prop"></span>
-          <span className="the-value"></span>
-        </span>
-        <span className="meteor-prop">
-          <span className="the-prop"></span>
-          <span className="the-value"></span>
-        </span>
-        <span className="the-prop"></span>
-        <progress></progress>
       </div>
     </aside>
   );
