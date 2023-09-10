efbe2949e43f59323ae60a48bda4c9c3089e0db6
CharanMN7_pc <maverickid247@gmail.com>
2023-09-11 03:09:31 +0530

    Update DataDisplayTable.jsx
    - Added local state variable and state manipulation functions to control the PopUp card
    - Passed necessary variable & function to the PopUp component

--- meteorhub/src/components/DataDisplayTable.jsx
+++ meteorhub/src/components/DataDisplayTable.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import "./DataDisplayTable.css";
+import PopUp from "./PopUp";
 
 export default function DataDisplayTable({ id, name, mass, recclass, year }) {
+  const [popUpState, setPopUpState] = useState("none");
+
+  function hidePopUp() {
+    setPopUpState("none");
+  }
+
+  function showPopUp() {
+    setPopUpState("flex");
+  }
   return (
-    <div className="data-row" key={id}>
-      <span className="left">
-        <span className="meteor-name">{name}</span>
-        <span className="meteor-mass">{mass} MT</span>
-      </span>
-      <span className="right">
-        <span className="meteor-recclass">{recclass}</span>
-        <span className="meteor-year">{year}</span>
-      </span>
-    </div>
+    <>
+      <div className="data-row" key={id} onClick={showPopUp}>
+        <span className="left">
+          <span className="meteor-name">{name}</span>
+          <span className="meteor-mass">{mass} MT</span>
+        </span>
+        <span className="right">
+          <span className="meteor-recclass">{recclass}</span>
+          <span className="meteor-year">{year}</span>
+        </span>
+      </div>
+      <PopUp
+        name={name}
+        mass={mass}
+        recclass={recclass}
+        year={year}
+        popUpState={popUpState}
+        hidePopUp={hidePopUp}
+      />
+    </>
   );
 }
